7e6cbffc248646ab773997f894509b4a5885bde9
Andy <obrezkov.andy@gmail.com>
2024-06-20 22:48:40 +0300

    Add adaptive design

--- js/index.js
+++ js/index.js
@@ -10,9 +10,9 @@ function changeArrowsRotation() {
   let hoursDeg = (hours * 5 + (Math.floor(minutes / 15))) * 6,
     minutesDeg = minutes * 6,
     secondsDeg = seconds * 6;
-  hoursNode.style.transform = `rotate(${hoursDeg}deg) translateX(-50%)`;
-  minutesNode.style.transform = `rotate(${minutesDeg}deg) translateX(-50%)`;
-  secondsNode.style.transform = `rotate(${secondsDeg}deg) translateX(-50%)`;
+  hoursNode.style.transform = `rotate(${hoursDeg}deg)`;
+  minutesNode.style.transform = `rotate(${minutesDeg}deg)`;
+  secondsNode.style.transform = `rotate(${secondsDeg}deg)`;
 }
 changeArrowsRotation();
 setInterval(changeArrowsRotation, 1000);
