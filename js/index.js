bd49b16cdd4de59c46d5d34a6564a4867b53dc9b
Andy <obrezkov.andy@gmail.com>
2024-06-21 23:40:54 +0300

    Add transition

--- js/index.js
+++ js/index.js
@@ -13,6 +13,11 @@ function changeArrowsRotation() {
   hoursNode.style.transform = `rotate(${hoursDeg}deg)`;
   minutesNode.style.transform = `rotate(${minutesDeg}deg)`;
   secondsNode.style.transform = `rotate(${secondsDeg}deg)`;
+  if (seconds == 0) {
+    secondsNode.classList.add('clock__hand_no-animation');
+  } else {
+    secondsNode.classList.remove('clock__hand_no-animation');
+  }
 }
 changeArrowsRotation();
 setInterval(changeArrowsRotation, 1000);
